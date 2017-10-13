a1c603304701da52e2d78dc2c0acc3fec53b3463
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-13 21:42:40 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ GROUP BY projects.title ORDER BY projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, pledges.amount FROM users
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users
 JOIN pledges ON users.id = pledges.user_id
 ORDER BY users.name"
 end
