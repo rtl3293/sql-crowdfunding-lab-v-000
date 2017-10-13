f5f2a1d7f80a8dd7b6417202e619edc5577360cd
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-13 21:25:48 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,10 +5,10 @@ CREATE TABLE projects (
   funding_goal INTEGER,
   start_date INTEGER,
   end_date INTEGER
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-)
+);
