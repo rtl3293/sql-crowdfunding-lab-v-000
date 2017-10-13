a600465eb0e2223f44a5c51eb960c1840c3eea42
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-13 21:53:47 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ GROUP BY users.name ORDER BY total_pledges"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT projects.category, pledges.amount
+"SELECT projects.category, pledges.amount FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 WHERE projects.category = 'music'"
 end
